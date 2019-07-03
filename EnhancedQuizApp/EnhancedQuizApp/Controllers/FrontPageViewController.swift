8c52387d22fbaeeec624847411cc78dcf1c69a0d
Sushi-UX <thechoygroup@gmail.com>
2019-07-02 22:51:35 -0400

    Created swift coding for the questions model
    Created swift coding for the questions model

---
+++ EnhancedQuizApp/EnhancedQuizApp/Controllers/FrontPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FrontPageViewController.swift
+//  EnhancedQuizApp
+//
+//  Created by Raymond Choy on 7/2/19.
+//  Copyright © 2019 thechoygroup. All rights reserved.
+//
+
+import UIKit
+
+class FrontPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EnhancedQuizApp/EnhancedQuizApp/Systems/Quiz.swift
@@ -0,0 +1,9 @@
+//
+//  Quiz.swift
+//  EnhancedQuizApp
+//
+//  Created by Raymond Choy on 7/2/19.
+//  Copyright © 2019 thechoygroup. All rights reserved.
+//
+
+import Foundation
